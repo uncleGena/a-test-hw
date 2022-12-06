308a07c23efb7d0a144e9bc389c554e92514c56a
KUDRIAVTSEV ANDREW <mail.andrew.k@gmail.com>
2022-12-06 22:16:34 +0100

    chore: setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  env: {
+    browser: true,
+    node: true,
+  },
+  rules: {
+    "prettier/prettier": "error",
+    curly: ["error", "multi-line"],
+    "@typescript-eslint/ban-ts-comment": "off",
+    // "vue/max-attributes-per-line": [
+    //   "error",
+    //   {
+    //     singleline: 1,
+    //     multiline: 1,
+    //   },
+    // ],
+    // 'space-before-function-paren': ["error", "never"],
+    "comma-dangle": ["error", "always-multiline"],
+    "no-console": "off",
+    "no-undef": "off",
+    "no-unused-vars": "off",
+    indent: "off",
+    "import/no-named-as-default": "off",
+    "@typescript-eslint/indent": ["error", 2],
+    "@typescript-eslint/no-unused-vars": "warn", // ["error"]
+    camelcase: ["off"],
+    "array-callback-return": "off",
+    "no-return-assign": "off",
+    "import/named": "off", // because of import { BigNumberish, BytesLike } from 'ethers'
+  },
+};
